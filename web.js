1fce4e8d9b8a88765f9f324fd15bb9d68df8ebc5
Don Liang <guodongliang@gmail.com>
2013-07-11 15:41:41 -0400

    print hello world from index.html

--- web.js
+++ web.js
@@ -1,12 +1,20 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+
+var indexHtml = fs.readFileSync('index.html', 'utf8');
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(indexHtml);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+	
+console.log(indexHtml);
+
